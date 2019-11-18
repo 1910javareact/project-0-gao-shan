7a9120281651f31b43918234e2c394208b13a2a7
gao-shan <sam.mosca@outlook.com>
2019-11-18 18:34:35 -0500

    update

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
 import express from 'express'
+import bodyparser from 'body-parser'
+import { userRouter } from './routers/user-router'
 
 const app = express()
+app.use(bodyparser.json())
+
+//this is where we put the requests to our routers
+
+// app.use('/login', loginRouter)
+app.use('/users', userRouter)
+
+
+app.listen(1001, ()=>{
+    console.log('app has started');   
+})
